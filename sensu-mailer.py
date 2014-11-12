35b9748392564a59ec8005fa339dcc6fb6bc57a5
Alex Leonhardt <aleonhardt.py@gmail.com>
2014-11-12 07:17:28 +0000

    split up generic functions from mailer to be able to re-use them, load settings from yaml, add e_smtp_host and _port to email settings, add a blank email_template for testing for now

--- sensu-mailer.py
+++ sensu-mailer.py
@@ -2,87 +2,42 @@
 
 
 import json
-import re
 import sys
 import smtplib
 from email.mime.text import MIMEText
 # from pprint import pprint
+import sensuutils
 
 
-def sanitize(data):
-    '''santize data/characters from alerts,etc.
-    that cannot be parsed as json'''
-
-    replace = {
-        '\n': ''
-    }
-
-    for key, val in replace.items():
-        data = re.sub(key, val, data)
-
-    return data
-
-
-def email(e_from='sensu', e_to=['root@localhost'], e_body=''):
+def email(e_from='sensu', e_to=['root@localhost'], e_body='',
+          e_smtp_host='localhost', e_smtp_port='25'):
     '''e_from = email from,
        e_to = email to,
-       e_body = email body'''
+       e_body = email body
+       e_smtp_host = smtp server to use
+       e_smtp_port = smtp port to use
+       '''
     msg = MIMEText(e_body)
     msg['Subject'] = 'Sensu notification'
     msg['From'] = e_from
     msg['To'] = ', '.join(e_to)
 
-    s = smtplib.SMTP('localhost')
+    s = smtplib.SMTP(host=e_smtp_host, port=e_smtp_port, timeout=10)
     s.sendmail(e_from, [e_to], msg.as_string())
     s.quit()
 
 
-def checkeventstate(event):
-    '''check state of the alert, occurrences, interval,
-    refreshes, stashes, etc'''
+def email_template(event):
+    '''create the email body based on this template
+    and return it as a string'''
 
-    defaults = {
-        'occurrences': 1,
-        'interval': 30,
-        'refresh': 600
-    }
+    email_body = """
+    SENSU NOTIFICATION\n
+    ==================\n
+    \n
+    """
 
-    if 'occurrences' in event.keys():
-        occurrences = event['occurrences']
-    else:
-        occurrences = defaults['occurrences']
-
-    if 'interval' in event.keys():
-        interval = event['check']['interval']
-    else:
-        interval = defaults['interval']
-
-    if 'refresh' in event.keys():
-        refresh = event['check']['refresh']
-    else:
-        refresh = defaults['refresh']
-
-    # some debugging for now
-    import time
-    try:
-        f = open('/tmp/checkeventstate.log', 'a')
-        f.write(str(event['client']['name']) + ': '
-                + str(event['check']['name'])
-                + '[' + str(time.time()) + ']: '
-                + str(event['occurrences']) + ' \n')
-        f.close()
-    except Exception as e:
-        sys.exit(e)
-
-    if occurrences > defaults['occurrences'] and event['action'] == 'create':
-        number = refresh / interval
-        if int(number) == 0 or event['occurrences'] % number == 0:
-            return True
-        else:
-            print('INFO: Only handling every __'
-                  + str(number) + '__ occurrences')
-
-    return False
+    return email_body
 
 
 def main(data):
@@ -92,15 +47,21 @@ def main(data):
     mail body simply converts the data from json/
     dictionary to a string'''
 
-    data = sanitize(data)
+    data = sensuutils.sanitize(data)
     event = json.loads(data)
 
-    if event['check']['name'] != 'keepalive':
-        if checkeventstate(event):
-            str_event = str(event)
-            email(e_body=str_event)
-    else:
-        print('skipped..')
+    settings = sensuutils.yamlconfig(filename='sensu-mailer.yaml')
+
+    e_to = settings['e_to']
+    e_from = settings['e_from']
+    e_smtp_host = settings['e_smtp_host']
+    e_smtp_port = settings['e_smtp_port']
+
+    if sensuutils.checkeventstate(event):
+        if sensuutils.checkstashes(event):
+            str_event = email_template(event)
+            email(e_from=e_from, e_to=e_to, e_body=str_event,
+                  e_smtp_host=e_smtp_host, e_smtp_port=e_smtp_port)
 
 
 if __name__ == "__main__":
---
+++ sensuutils.py
@@ -0,0 +1,103 @@
+#! /usr/bin/env python
+
+import re
+import yaml
+
+
+def yamlconfig(filename='settings.yaml'):
+    '''reads config from yaml file in
+    same directory as this file
+
+    expected format (must start with settings: ) :
+
+    ---
+    settings:
+        key: value
+        key2: value2
+        etc.
+
+    example:
+
+    ---
+    settings:
+        e_from: "sensu@mailer.com"
+        e_to: "alerts@mailer.com"
+        e_smtp_host: "127.0.0.1"
+        e_smtp_port: "25"
+
+    keys/values depend on requirement of each individual
+    module - e.g. sensu-mailer.py should specify what
+    file to read and what to expect in the config file
+    '''
+
+    try:
+        with open(filename) as f:
+            config = yaml.load(f.read())
+    except Exception as e:
+        print('Exception: ' + e)
+
+    if type(config) is dict:
+        if 'settings' not in config.items():
+            print('Error: config file does not start with "settings: "')
+            return {}
+    else:
+        return {}
+
+    return config
+
+
+def sanitize(data):
+    '''santize data/characters from alerts,etc.
+    that cannot be parsed as json'''
+
+    replace = {
+        '\n': ''
+    }
+
+    for key, val in replace.items():
+        data = re.sub(key, val, data)
+
+    return data
+
+
+def checkeventstate(event):
+    '''check state of the alert, occurrences, interval,
+    refreshes, stashes, etc'''
+
+    defaults = {
+        'occurrences': 1,
+        'interval': 30,
+        'refresh': 600
+    }
+
+    if 'occurrences' in event.keys():
+        occurrences = event['occurrences']
+    else:
+        occurrences = defaults['occurrences']
+
+    if 'interval' in event.keys():
+        interval = event['check']['interval']
+    else:
+        interval = defaults['interval']
+
+    if 'refresh' in event.keys():
+        refresh = event['check']['refresh']
+    else:
+        refresh = defaults['refresh']
+
+    if occurrences > defaults['occurrences'] and event['action'] == 'create':
+        number = refresh / interval
+        if int(number) == 0 or event['occurrences'] % number == 0:
+            return True
+        else:
+            print('INFO: Only handling every __'
+                  + str(number) + '__ occurrences')
+
+    return False
+
+
+def checkstashes(event):
+    '''checks sensu stashes, will return False if a stash
+    was found so that no alert will be sent'''
+
+    return True
